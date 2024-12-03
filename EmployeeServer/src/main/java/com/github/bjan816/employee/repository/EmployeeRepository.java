3b579e5058a72b2e7933dbcdfbe154ecb2150203
bjan816 <bjan816@aucklanduni.ac.nz>
2024-12-04 00:25:38 +0900

    Update EmployeeRepository.java

--- EmployeeServer/src/main/java/com/github/bjan816/employee/repository/EmployeeRepository.java
+++ EmployeeServer/src/main/java/com/github/bjan816/employee/repository/EmployeeRepository.java
@@ -1,5 +1,6 @@
 package com.github.bjan816.employee.repository;
 
+import com.github.bjan816.employee.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
