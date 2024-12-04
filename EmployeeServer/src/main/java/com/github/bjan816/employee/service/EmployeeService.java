aed59c9c1280baa9f31364d8d05683b2c2e27538
bjan816 <bjan816@aucklanduni.ac.nz>
2024-12-04 18:13:48 +0900

    Update EmployeeService.java

--- EmployeeServer/src/main/java/com/github/bjan816/employee/service/EmployeeService.java
+++ EmployeeServer/src/main/java/com/github/bjan816/employee/service/EmployeeService.java
@@ -1,7 +1,17 @@
 package com.github.bjan816.employee.service;
 
+import com.github.bjan816.employee.entity.Employee;
+import com.github.bjan816.employee.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public Employee postEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
 }
