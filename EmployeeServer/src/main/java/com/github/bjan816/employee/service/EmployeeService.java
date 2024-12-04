2c97f7760ff1de615486eaef15486bd314e9c18a
bjan816 <bjan816@aucklanduni.ac.nz>
2024-12-05 00:08:33 +0900

    Use manual constructor due to issues with lombok

--- EmployeeServer/src/main/java/com/github/bjan816/employee/service/EmployeeService.java
+++ EmployeeServer/src/main/java/com/github/bjan816/employee/service/EmployeeService.java
@@ -6,10 +6,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
+// @RequiredArgsConstructor
 public class EmployeeService {
 
     private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
 
     public Employee postEmployee(Employee employee) {
         return employeeRepository.save(employee);
